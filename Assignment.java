5e0ab70c21bb8f1f9f20a93865a7c13969851fae
phebsv <phebsvillaflor@gmail.com>
2025-05-19 14:19:36 +0800

    Replace toString() with toFileString() method

--- Assignment.java
+++ Assignment.java
@@ -78,7 +78,7 @@ public class Assignment {
 	// Returns string representation of assignment with it's title, description, and
 	// formatted due date
 	@Override
-	public String toString() {
+	public String toFileString() {
 		return "Assignment {" +
 				"Title='" + title + '\'' +
 				", Description='" + description + '\'' +
