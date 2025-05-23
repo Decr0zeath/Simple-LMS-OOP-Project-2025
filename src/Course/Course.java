884bdfe25b03068b1e4484b8e508abe8889fd200
Keinth Durano <keinthnino.durano.23@usjr.edu.ph>
2025-05-23 15:38:51 +0800

    ASDASD

--- src/Course/Course.java
+++ src/Course/Course.java
@@ -1,59 +1,58 @@
-public class Course {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SubjectInfo {
     private String courseId;
     private String courseName;
-    private String teacher;
-    private List<String> enrolledStudents;
+    private List<TeacherInfo> teachers;
+    private List<StudentInfo> enrolledStudents;
 
-    public Course(String courseId, String courseName, String teacher) {
+    public SubjectInfo(String courseId, String courseName, TeacherInfo... teachers) {
         this.courseId = courseId;
         this.courseName = courseName;
-        this.teacher = teacher;
-        this.enrolledStudents = new ArrayList<>();
+        this.teachers = Arrays.asList(teachers);
+         this.enrolledStudents = new ArrayList<>();
     }
 
     public String getCourseId() {
         return courseId;
     }
 
-    public void setCourseId(String courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getCourseName() {
+    public String getCourseName() {     
         return courseName;
     }
 
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
+    public List<TeacherInfo> getTeachers() {
+        return teachers;
     }
-
-    public String getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(String teacher) {
-        this.teacher = teacher;
-    }
-
-    public List<String> getEnrolledStudents() {
+    public List<StudentInfo> getEnrolledStudents() {
         return enrolledStudents;
     }
-
-    public void enrollStudent(String studentName) {
-        enrolledStudents.add(studentName);
+    public void enrollStudent(StudentInfo student) {
+        enrolledStudents.add(student);
     }
 
-    public void removeStudent(String studentName) {
-        enrolledStudents.remove(studentName);
-    }
+   public String toFileString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(courseId).append(",").append(courseName).append(", Teachers: ");
+        for (int i = 0; i < teachers.size(); i++) {
+            TeacherInfo t = teachers.get(i);
+            sb.append(t.getFirstName()).append(" ").append(t.getLastName());
+            if (i < teachers.size() - 1) sb.append(" | ");
+        }
 
-    @Override
-    public String toString() {
-        return "Course{" +
-                "courseId = '" + courseId + '\'' +
-                ", courseName = '" + courseName + '\'' +
-                ", teacher = '" + teacher + '\'' +
-                ", enrolled Students = " + enrolledStudents +
-                '}';
+        sb.append(", Enrolled Students: ");
+        if (enrolledStudents.isEmpty()) {
+            sb.append("None");
+        } else {
+            for (int i = 0; i < enrolledStudents.size(); i++) {
+                StudentInfo s = enrolledStudents.get(i);
+                sb.append(s.getFirstName()).append(" ").append(s.getLastName());
+                if (i < enrolledStudents.size() - 1) sb.append(" | ");
+            }
+        }
+
+        return sb.toString();
     }
 }
