f42f8d023bd6b1a2902ff70fb406d6e9c7fc7dc3
Raven Earl Nacua <ravennacua2@gmail.com>
2025-05-27 13:13:27 +0800

    CHANGED TEACHERCLASS

--- src/StudentTeacher/Teacher.java
+++ src/StudentTeacher/Teacher.java
@@ -3,21 +3,11 @@ package StudentTeacher;
 import User.User;
 
 public class Teacher extends User{
-    private String course;
-    public Teacher(String firstName, String lastName, String accountID, String password, String course) {
+    public Teacher(String firstName, String lastName, String accountID, String password) {
         super(firstName, lastName, accountID, password, "Teacher");
-        this.course = course;
-    }
 
-    public String getCourse() {
-        return course;
     }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
     public String toFileString() {
-        return firstName + "," + lastName + "," + accountID + "," + password + "," + course;
+        return firstName + "," + lastName + "," + accountID + "," + password;
     }
 }
