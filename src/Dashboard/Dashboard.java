4754a2cb208afb51a30d8eea49c1dd18a1dc5276
BrianJoseph0422 <brianjoespharatia@gmail.com>
2025-05-27 14:02:25 +0800

    Connected LoginScreen and Dashboard

--- src/Dashboard/Dashboard.java
+++ src/Dashboard/Dashboard.java
@@ -250,4 +250,9 @@ public class Dashboard {
         Image img = icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
         return new ImageIcon(img);
     }
+
+    public void setVisible(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+    }
 }
